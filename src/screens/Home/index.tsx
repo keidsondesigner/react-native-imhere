93e3b34b98e1599a741f471d2debac67de3c7099
keidson designer <keidsondesigner@gmail.com>
2024-01-29 11:09:38 -0400

    feat: :sparkles: criação do form do participante

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './styles';
+import { Participant } from '../../components/Participant';
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Adicionar');
+  } 
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Hello world
+      </Text>
+      <Text style={styles.eventDate}>
+        Segunda, 29 de Janeiro de 2024.
+      </Text>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Participant />
+    </View>
+  );
+}
